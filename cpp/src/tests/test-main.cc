0e74d25a2cbad339304273867f12f525eb79bc53
Dmitri Nikulin <dnikulin@gmail.com>
2010-03-09 11:16:55 +1100

    C++: Create unit test skeleton

---
+++ cpp/src/tests/test-main.cc
@@ -0,0 +1,7 @@
+// Copyright (c) 2009 Dmitri Nikulin
+// See LICENSE-MIT.txt for details
+// Part of the PPK project
+
+// Sufficient to generate main() for static library linking
+#define BOOST_TEST_MODULE ppk unit tests
+#include <boost/test/unit_test.hpp>
